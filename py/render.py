de48a879eb84a76aeb1dea2d214f5fb02f497647
Phong Nguyen <phong204n@gmail.com>
2025-03-07 14:46:55 -0800

    Added basic HTML and CSS framework

--- py/render.py
+++ py/render.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 ##  Begin Local Imports
 import resources
 
-app = Flask(__name__, template_folder=str(resources.CONST_FRONTEND_DIR))
+app = Flask(__name__, template_folder=str(resources.CONST_FRONTEND_DIR), static_folder=str(resources.CONST_ROOT_DIR))
 
 @app.route("/")
 def render_index():
